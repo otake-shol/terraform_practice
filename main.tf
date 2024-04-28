588a9bb1cc7c2adf5f4503afeb2a13a06f21c2da
otkshol <charm.ot.trog@gmail.com>
2024-04-28 15:52:01 +0900

    update: テンプレート内容にファイル修正

--- main.tf
+++ main.tf
@@ -1,21 +1,31 @@
-provider "aws" {
-  profile =  "terraform"
-  region = "ap-northeast-1"
-}
-
-resource "aws_instance" "hello-world" {
-  ami = "ami-04e0b6d6cfa432943"
-  instance_type = "t2.micro"
-
-  tags =  {
-    Name = "hellowolrd"
+# ---------------------------------------------
+# Terraform configuration
+# ---------------------------------------------
+terraform {
+  required_version = ">=0.13"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
   }
-
-  user_data = <<EOF
-#!/bin/bash
-amazon-linux-extras install -y nginx1.12
-systemctl start nginx
-EOF
 }
 
+# ---------------------------------------------
+# Provider
+# ---------------------------------------------
+provider "aws" {
+  profile = "terraform"
+  region  = "ap-northeast-1"
+}
 
+# ---------------------------------------------
+# Variables
+# ---------------------------------------------
+variable "project" {
+  type = string
+}
+
+variable "environment" {
+  type = string
+}
