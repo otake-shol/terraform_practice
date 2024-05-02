703794e856731fcce38626b3f2b236b40f9a1916
otkshol <charm.ot.trog@gmail.com>
2024-05-02 17:21:42 +0900

    add: rdsの作成

--- rds.tf
+++ rds.tf
@@ -51,3 +51,49 @@ resource "random_string" "db_password" {
   length  = 16
   special = false
 }
+
+resource "aws_db_instance" "mysql_standalone" {
+  engine         = "mysql"
+  engine_version = "8.0.36"
+
+  identifier = "${var.project}-${var.environment}-mysql-standalone"
+
+  username = "admin"
+  password = random_string.db_password.result
+
+  // t2.microは2024年3月29日に廃止
+  // （参考） https://repost.aws/ja/articles/AR_Rw2Xbe5Q9O1ZPKbU5S4Mg/amazon-rds-for-sql-server-is-ending-support-for-m4-r4-and-t2-database-instances-on-march-29-2024
+  instance_class = "db.t3.micro"
+
+  allocated_storage     = 20
+  max_allocated_storage = 50
+  storage_type          = "gp2"
+  storage_encrypted     = false
+
+  multi_az               = false
+  availability_zone      = "ap-northeast-1a"
+  db_subnet_group_name   = aws_db_subnet_group.mysql_standalone_subnet_group.name
+  vpc_security_group_ids = [aws_security_group.db_sg.id]
+  publicly_accessible    = false
+  port                   = 3306
+
+  name                 = "tastylog"
+  parameter_group_name = aws_db_parameter_group.mysql_standalone_parameter_group.name
+  option_group_name    = aws_db_option_group.mysql_standalone_option_group.name
+
+  backup_window              = "04:00-05:00"
+  backup_retention_period    = 7
+  maintenance_window         = "Mon:05:00-Mon:08:00"
+  auto_minor_version_upgrade = false
+
+  deletion_protection = true
+  skip_final_snapshot = false
+
+  apply_immediately = true
+
+  tags = {
+    Name    = "${var.project}-${var.environment}-mysql-standalone"
+    Project = var.project
+    Env     = var.environment
+  }
+}
