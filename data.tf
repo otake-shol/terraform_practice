400156dd9ff14a7724d9ce94a030c061eddff6a8
otkshol <charm.ot.trog@gmail.com>
2024-05-03 03:02:11 +0900

    add: aws_amiの検索を追加

--- data.tf
+++ data.tf
@@ -1,3 +1,21 @@
 data "aws_prefix_list" "s3_pl" {
   name = "com.amazonaws.*.s3"
 }
+
+data "aws_ami" "app" {
+  most_recent = true
+  owners      = ["self", "amazon"]
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami-2023.4.20240429.0-kernel-6.1-x86_64"]
+  }
+  filter {
+    name   = "Root-device-name"
+    values = ["ebs"]
+  }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
