78ce8408112d97f2e34805ca0b1e6803f77dacb4
otkshol <charm.ot.trog@gmail.com>
2024-05-04 03:31:30 +0900

    add: APサーバー用 EC2を作成

--- appserver.tf
+++ appserver.tf
@@ -11,3 +11,24 @@ resource "aws_key_pair" "keypair" {
     Env     = var.environment
   }
 }
+
+# ---------------------------------------------
+# EC2 Instance
+# ---------------------------------------------
+resource "aws_instance" "app_server" {
+  ami           = data.aws_ami.app.id
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.public_subnet_1a.id
+  vpc_security_group_ids = [
+    aws_security_group.app_sg.id,
+    aws_security_group.opmng_sg.id
+  ]
+  key_name = aws_key_pair.keypair.key_name
+
+  tags = {
+    Name    = "${var.project}-${var.environment}-keypair"
+    Project = var.project
+    Env     = var.environment
+    Type    = "app"
+  }
+}
