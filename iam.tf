f8ee4d8a42055a8f4c2dd7547ae4da4b638e1e2a
otkshol <charm.ot.trog@gmail.com>
2024-05-06 22:44:04 +0900

    add: IAMロールの追加

--- iam.tf
+++ iam.tf
@@ -1,3 +1,13 @@
+# ---------------------------------------------
+# IAM Role
+# ---------------------------------------------
+
+resource "aws_iam_role" "app_iam_role" {
+  name               = "${var.project}-${var.environment}-aws_iam_role"
+  assume_role_policy = data.aws_iam_policy_document.ec2_assume_role.json
+
+}
+
 data "aws_iam_policy_document" "ec2_assume_role" {
   statement {
     actions = ["sts:AssumeRole"]
