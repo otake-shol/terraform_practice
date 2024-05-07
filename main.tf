0306c089e3bc66bf01037221202438b0456afbcc
otkshol <charm.ot.trog@gmail.com>
2024-05-08 08:25:58 +0900

    practice: メタ引数foreachを用いた複数VPC作成

--- main.tf
+++ main.tf
@@ -45,3 +45,25 @@ resource "aws_iam_user" "user" {
   count = 2
   name  = "testuser-${count.index}"
 }
+
+# ---------------------------------------------
+# VPCの複数作成
+# ---------------------------------------------
+// メタ引数利用の練習が目的。本質的に不要な設定
+resource "aws_vpc" "vpc" {
+  cidr_block = "192.168.0.0/20"
+}
+
+resource "aws_subnet" "subnet" {
+  for_each = {
+    "192.168.1.0/24" = "ap-northeast-1a"
+    "192.168.2.0/24" = "ap-northeast-1c"
+    "192.168.3.0/24" = "ap-northeast-1d"
+  }
+
+  vpc_id            = aws_vpc.vpc.id
+  cidr_block        = each.key
+  availability_zone = each.value
+
+}
+
