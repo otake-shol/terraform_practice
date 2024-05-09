0d992f6801366927efe556472fe0566636acafea
otkshol <charm.ot.trog@gmail.com>
2024-05-10 00:28:20 +0900

    add: ターゲットグループの作成

--- elb.tf
+++ elb.tf
@@ -14,3 +14,24 @@ resource "aws_lb" "alb" {
   ]
 
 }
+
+# ---------------------------------------------
+# target group
+# ---------------------------------------------
+resource "aws_lb_target_group" "alb_target_group" {
+  name     = "${var.project}-${var.environment}-app-tg"
+  port     = 3000
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.vpc.id
+
+  tags = {
+    Name    = "${var.project}-${var.environment}-app-tg"
+    project = var.project
+    Env     = var.environment
+  }
+}
+
+resource "aws_lb_target_group_attachment" "aws_lb_target_group_attachment" {
+  target_group_arn = aws_lb_target_group.alb_target_group.arn
+  target_id        = aws_instance.app_server.id
+}
